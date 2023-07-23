4b345f8f97806fec8c5a1441bd9a15de946d3fb2
Stephen Bird <sebirdman@gmail.com>
2023-07-23 20:31:55 +0000

    Sigmastar Uboot Changes
    Set env to match stock miyoo mini plus
    
    Undo Bootm Sigmastar Change
    
    Fix location of uboot to before uboot
    
    Undo unnecessary changes to part_dos
    
    Undo Disk Part Changes
    
    Remove fastboot support
    
    Remove need for lzo changes
    
    Revert boot environment back to near stock
    
    Undo net changes
    
    Undo nonchange to usb
    
    Remove change to add unused define
    
    Remove unused define
    
    Remove unneeded commands
    
    Remove unneeded changes
    
    Undo nand changes since we don't have a nand
    
    Undo usb changes since we don't have USB wired to the USB port
    
    Undo unused kconfig change
    
    Remove unused files
    
    Cleanup build process
    
    Undo fix for free
    
    Remove more unused components
    
    Remove versioning schema
    
    Remove unused mmc commands
    
    Remove USB
    
    Remove more unneeded commands
    
    Make fatload not dependant on USB support
    
    Revert more random items
    
    Remove unneeded files
    
    Revert changes as wdt isn't enabled by us anyways
    
    Cleanup warnings
    
    Revert to only add bootlogo here
    
    Cleanup commands
    
    Remove version check that doesn't work properly
    
    Continue pruning

--- common/autoboot.c
+++ common/autoboot.c
@@ -11,7 +11,25 @@
 #include <cli.h>
 #include <fdtdec.h>
 #include <menu.h>
+#include <malloc.h>
 #include <post.h>
+#ifdef CONFIG_SSTAR_IR
+#include <ms_ir.h>
+#endif
+
+#if defined(CONFIG_MS_PARTITION)
+#include "part_mxp.h"
+#endif
+
+
+#if defined(CONFIG_CMD_MTDPARTS)
+#include <jffs2/jffs2.h>
+/* partition handling routines */
+int mtdparts_init(void);
+int find_dev_and_part(const char *id, struct mtd_device **dev,
+        u8 *part_num, struct part_info **part);
+#endif
+
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -34,7 +52,7 @@ static int stored_bootdelay;
 static int abortboot_keyed(int bootdelay)
 {
 	int abort = 0;
-	uint64_t etime = endtick(bootdelay);
+	uint64_t etime = 2;//endtick(bootdelay);
 	struct {
 		char *str;
 		u_int len;
@@ -42,12 +60,12 @@ static int abortboot_keyed(int bootdelay)
 	}
 	delaykey[] = {
 		{ .str = getenv("bootdelaykey"),  .retry = 1 },
-		{ .str = getenv("bootdelaykey2"), .retry = 1 },
-		{ .str = getenv("bootstopkey"),   .retry = 0 },
-		{ .str = getenv("bootstopkey2"),  .retry = 0 },
+//		{ .str = getenv("bootdelaykey2"), .retry = 1 },
+//		{ .str = getenv("bootstopkey"),   .retry = 0 },
+//		{ .str = getenv("bootstopkey2"),  .retry = 0 },
 	};
 
-	char presskey[MAX_DELAY_STOP_STR];
+	char presskey[MAX_DELAY_STOP_STR]={0};
 	u_int presskey_len = 0;
 	u_int presskey_max = 0;
 	u_int i;
@@ -65,6 +83,7 @@ static int abortboot_keyed(int bootdelay)
 	if (delaykey[0].str == NULL)
 		delaykey[0].str = CONFIG_AUTOBOOT_DELAY_STR;
 #  endif
+#if 0
 #  ifdef CONFIG_AUTOBOOT_DELAY_STR2
 	if (delaykey[1].str == NULL)
 		delaykey[1].str = CONFIG_AUTOBOOT_DELAY_STR2;
@@ -77,7 +96,7 @@ static int abortboot_keyed(int bootdelay)
 	if (delaykey[3].str == NULL)
 		delaykey[3].str = CONFIG_AUTOBOOT_STOP_STR2;
 #  endif
-
+#endif
 	for (i = 0; i < sizeof(delaykey) / sizeof(delaykey[0]); i++) {
 		delaykey[i].len = delaykey[i].str == NULL ?
 				    0 : strlen(delaykey[i].str);
@@ -123,16 +142,22 @@ static int abortboot_keyed(int bootdelay)
 				abort = 1;
 			}
 		}
-	} while (!abort && get_ticks() <= etime);
+		etime--;
+	} while (!abort && etime);
 
+#if 0// if DEBUG_BOOTKEYS
 	if (!abort)
 		debug_bootkeys("key timeout\n");
+
+#endif
 
 #ifdef CONFIG_SILENT_CONSOLE
 	if (abort)
 		gd->flags &= ~GD_FLG_SILENT;
 #endif
-
+	if (tstc()) {
+	    (void) getc();  /* consume input	*/
+	}
 	return abort;
 }
 
@@ -270,7 +295,53 @@ const char *bootdelay_process(void)
 		s = getenv("altbootcmd");
 	} else
 #endif /* CONFIG_BOOTCOUNT_LIMIT */
-		s = getenv("bootcmd");
+        char* boot_cmd_line = getenv("bootcmd");
+        char* env = getenv("use_bak_rootfs");
+
+        if(!strcmp(env, "1") && (strstr(boot_cmd_line,"rootfs_bak") == NULL))//replace rootfs with rootfs_bak
+        {
+            char *str_start = strstr(boot_cmd_line, "rootfs");
+            if(str_start)
+            {
+                int num_start_bytes = str_start - boot_cmd_line;
+                int cmd_len = strlen(boot_cmd_line);
+                int str1_len = strlen("_bak");
+                int str2_len = strlen("rootfs");
+
+                char *tmp_buf = malloc(cmd_len + str1_len);
+                strncpy(tmp_buf, boot_cmd_line, num_start_bytes);
+                strcpy(tmp_buf + num_start_bytes, "rootfs_bak");
+                strncpy(tmp_buf + num_start_bytes + str1_len + str2_len, str_start + str2_len,cmd_len - num_start_bytes - str2_len);
+                tmp_buf[cmd_len + str1_len] = '\0';
+                setenv("bootcmd", tmp_buf);
+                saveenv();
+                s = tmp_buf;
+            }
+        }
+        else if(!strcmp(env, "0") && (strstr(boot_cmd_line,"rootfs_bak") != NULL)) //replace rootfs_bak with rootfs
+        {
+            char *str_start = strstr(boot_cmd_line, "rootfs_bak");
+            if(str_start)
+            {
+                int num_start_bytes = str_start - boot_cmd_line;
+                int cmd_len = strlen(boot_cmd_line);
+                int str1_len = strlen("_bak");
+                int str2_len = strlen("rootfs");
+                char *tmp_buf = malloc(cmd_len);
+
+                strncpy(tmp_buf, boot_cmd_line, num_start_bytes + str2_len);
+                strncpy(tmp_buf + num_start_bytes + strlen("rootfs"),str_start + str1_len + str2_len,cmd_len - num_start_bytes - str1_len - str2_len);
+
+                tmp_buf[cmd_len - str1_len] = '\0';
+                setenv("bootcmd", tmp_buf);
+                saveenv();
+                s = tmp_buf;
+            }
+        }
+        else
+        {
+            s = boot_cmd_line;
+        }
 
 	process_fdt_options(gd->fdt_blob);
 	stored_bootdelay = bootdelay;
@@ -278,8 +349,69 @@ const char *bootdelay_process(void)
 	return s;
 }
 
+int get_partition_size(char* strENVName)
+{
+        u32    size;
+#if defined(CONFIG_CMD_MTDPARTS) || defined(CONFIG_MS_SPINAND)
+
+        struct mtd_device *dev;
+        struct part_info *part;
+        u8 pnum;
+        int ret;
+
+        ret = mtdparts_init();
+        if (ret)
+            return ret;
+
+        ret = find_dev_and_part(strENVName, &dev, &pnum, &part);
+        if (ret)
+        {
+            debug("failed to get_partition_size with name: %s\n", strENVName);
+            return ret;
+        }
+
+        if (dev->id->type != MTD_DEV_TYPE_NAND)
+        {
+            puts("not a NAND device\n");
+            return -1;
+        }
+
+        size = part->size;
+#elif defined(CONFIG_MS_PARTITION)
+        u32 idx;
+
+        mxp_record rec;
+        mxp_load_table();
+        idx=mxp_get_record_index(strENVName);
+        if(idx<0)
+        {
+            debug("can not found mxp record: %s\n", strENVName);
+            return 0;
+        }
+
+        if(0 != mxp_get_record_by_index(idx,&rec))
+        {
+            debug("failed to get_partition_size with name: %s\n", strENVName);
+            return 0;
+        }
+        size = rec.size;
+#else
+        size = 0x00680000;
+#endif
+        return size;
+
+}
+
 void autoboot_command(const char *s)
 {
+#if defined(CONFIG_MS_USB) || defined(CONFIG_MS_SDMMC) || defined(CONFIG_MS_EMMC)
+    char *env;
+#endif
+
+#ifdef CONFIG_SSTAR_IR
+    u8 key_value = 0;
+#endif
+
 	debug("### main_loop: bootcmd=\"%s\"\n", s ? s : "<UNDEFINED>");
 
 	if (stored_bootdelay != -1 && s && !abortboot(stored_bootdelay)) {
@@ -287,12 +419,82 @@ void autoboot_command(const char *s)
 		int prev = disable_ctrlc(1);	/* disable Control C checking */
 #endif
 
+#ifdef CONFIG_CMD_CUSTOMER_INIT
+		run_command("customer_init", 0);
+#endif
+
+#ifdef CONFIG_CMD_NETUPGRADE
+		run_command("net_upgrade", 0);
+#endif
+#if defined(CONFIG_MS_USB)
+        env = getenv("usbautoupgrade");
+        if(!strcmp(env, "1"))
+        {
+            run_command("setenv usbautoupgrade 2", 0);
+            run_command("saveenv",0);
+            run_command("usbstar", 0);
+        }
+        if(!strcmp(env, "2"))
+        {
+            run_command("setenv usbautoupgrade 1", 0);
+            run_command("saveenv",0);
+        }
+
+#endif
+#if defined(CONFIG_MS_SDMMC)
+        env = getenv("sdautoupgrade");
+        if(!strcmp(env, "1"))
+        {
+            run_command("setenv sdautoupgrade 2", 0);
+            run_command("saveenv",0);
+            run_command("sdstar", 0);
+        }
+        if(!strcmp(env, "2"))
+        {
+            run_command("setenv sdautoupgrade 1", 0);
+            run_command("saveenv",0);
+        }
+#endif
+#if defined(CONFIG_MS_EMMC)
+        env = getenv("emmcautoupgrade");
+        if(!strcmp(env, "1"))
+        {
+            run_command("setenv emmcautoupgrade 2", 0);
+            run_command("saveenv",0);
+            run_command("emmcstar", 0);
+        }
+        if(!strcmp(env, "2"))
+        {
+            run_command("setenv emmcautoupgrade 1", 0);
+            run_command("saveenv",0);
+        }
+#endif
+
+#ifdef CONFIG_SSTAR_IR
+        _mdrv_ir_init();
+
+        while(1){
+            //mdelay(100);
+            if(!_mdrv_get_key(&key_value)){
+                if(key_value == START_UP){
+                    break;
+                }
+            }
+        }
+#endif
+		dcache_enable();
+		icache_enable();
+
 		run_command_list(s, -1, 0);
+
+		dcache_disable();
+		icache_disable();
 
 #if defined(CONFIG_AUTOBOOT_KEYED) && !defined(CONFIG_AUTOBOOT_KEYED_CTRLC)
 		disable_ctrlc(prev);	/* restore Control C checking */
 #endif
 	}
+
 
 #ifdef CONFIG_MENUKEY
 	if (menukey == CONFIG_MENUKEY) {
